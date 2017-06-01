d0ec5cd4169df94a179287dcf753d56dd18c284c
Riss <rissgadi@gmail.com>
2017-06-01 13:51:59 -0500

    completed checkpoint-29

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,18 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
-  # #7
+  # #4 because we don't want to create any /possts/:id routes,
+  # just posts/:post_id/comments routes
+  resources :posts, only: [] do
+
+  # #5
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
 
-# #2
 get 'about' => 'welcome#about'
 
   root 'welcome#index'
